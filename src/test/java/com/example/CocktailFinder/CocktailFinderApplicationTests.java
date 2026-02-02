d8fcfa469b8436c3f43bf714b83c16f1d4b9314f
Afreeman <79459307+Andygurl5566@users.noreply.github.com>
2026-02-01 22:18:17 -0600

    first commit

---
+++ src/test/java/com/example/CocktailFinder/CocktailFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CocktailFinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocktailFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
